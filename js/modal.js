e0db48fa9d27bc90dfff99eb15edbdb393a905c0
ollidm <manea419@gmail.com>
2023-08-09 00:51:34 +0300

    d

--- js/modal.js
+++ js/modal.js
@@ -4,8 +4,10 @@
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
+
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
   }
