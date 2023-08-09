4fc1f434d2210745df4dface499b7a7ca3abbe15
ollidm <manea419@gmail.com>
2023-08-09 20:03:04 +0300

    1

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,15 +1,16 @@
 (() => {
-    const refs = {
-      openMenuBtn: document.querySelector("[data-menu-open]"),
-      closeMenuBtn: document.querySelector("[data-menu-close]"),
-      menu: document.querySelector("[data-menu]"),
-    };
-  
-    refs.openMenuBtn.addEventListener("click", toggleMenu);
-    refs.closeMenuBtn.addEventListener("click", toggleMenu);
-  
-    function toggleMenu() {
-      refs.menu.classList.toggle("is-hidden");
-      document.body.classList.toggle("no-scroll")
-    }
-  })();
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    document.body.classList.toggle('scroll-lock');
+    refs.menu.classList.toggle('burger-open');
+    refs.openMenuBtn.classList.toggle('burger-open');
+  }
+})();
--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,17 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    modalBody: document.querySelector('[data-modal-body]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle('scroll-lock');
+    refs.modal.classList.toggle('is-hidden');
+    refs.modalBody.classList.toggle('is-hidden');
   }
 })();
